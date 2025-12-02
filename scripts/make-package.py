5a71b4bcb4a5a2b62aca91613c82666adfae9d1f
Dmitry Volodin <dv@nocproject.org>
2025-12-02 14:16:57 +0100

    npm publish

---
+++ scripts/make-package.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------
+# Gufo Font: Create package.json file.
+# ---------------------------------------------------------------------
+# Copyright (C) 2025, Gufo Labs
+# ---------------------------------------------------------------------
+"""Generate build/_codepoints.scss file."""
+
+# Python modules
+import json
+from pathlib import Path
+
+# Gufo Font modules
+from gufo.font.manifest import Manifest
+
+DIST = Path("dist", "gufo-font")
+PACKAGE_JSON = DIST / "package.json"
+
+
+def main() -> None:
+    """Generate packages.json file."""
+    manifest = Manifest.read()
+    PACKAGE_JSON.write_text(json.dumps(manifest.package_json, indent=2))
+
+
+if __name__ == "__main__":
+    main()
